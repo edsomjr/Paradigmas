0229479f0f9893bf20d8742bd4b242a6378cd6be
Edson Alves <edsomjr@gmail.com>
2020-01-31 22:06:48 -0300

    Adição da seção sobre variáveis e início da seção sobre I/O.

---
+++ Programacao_Estruturada/slides/conceitos_elementares/codes/maratona.f90
@@ -0,0 +1,15 @@
+! Verifica se um competidor pode ou não participar da Maratona
+! no ano de 2020
+program maratona
+
+    implicit none
+
+    integer :: inicio = 2017, nascimento = 1995
+    logical :: primeira_graduacao = .true.
+    logical :: ok
+    
+    ok = nascimento >= 1997 .or. (primeira_graduacao .and. inicio > 2015)
+
+    write(*,*) 'Pode participar? ', ok
+
+end program maratona
---
+++ Programacao_Estruturada/slides/conceitos_elementares/codes/polar.f90
@@ -0,0 +1,19 @@
+! Computa a forma polar do complexo c
+program polar
+
+    implicit none
+
+    complex :: c = complex (0.5, sqrt(3.0)/2)
+
+    real :: p, theta        ! Parâmetros da forma polar
+
+    write(*,*) 'c = ', c
+
+    ! Converte c para a forma polar c = p(cos(theta) + isen(theta))
+    p = sqrt(real(c) ** 2 + aimag(c) ** 2)
+    theta = atan(aimag(c)/real(c))
+
+    ! p = 1, theta = pi/3 = 60°
+    write(*,*) 'p = ', p, ' theta = ', theta
+
+end program polar
