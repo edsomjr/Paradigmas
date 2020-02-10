89252ecbac12fef693f914565b402710112fda58
Edson Alves <edsomjr@gmail.com>
2020-02-10 16:48:15 -0300

    Adição do texto sobre módulos.

---
+++ Programacao_Estruturada/slides/estruturas_e_funcoes/codes/fibonacci.f90
@@ -0,0 +1,32 @@
+! Obtém os n próximos números de Fibonacci
+program fibonacci
+
+    write(*,1) next_fib(8, .false.)
+    write(*,2) next_fib(5, .false.)
+    write(*,3) next_fib(10, .true.)
+
+1   format ('8 primeiros números de Fibonacci:', 8I3)
+2   format ('5 próximos números de Fibonacci:', 5I4)
+3   format ('10 primeiros números de Fibonacci:', 10I3)
+
+contains
+
+    function next_fib(n, reset)
+        integer :: n, i, next_fib(n)
+        logical :: reset
+        integer, save :: a = 0, b = 1
+
+        if (reset) then
+            a = 0
+            b = 1
+        end if
+
+        do i = 1, n 
+            next_fib(i) = b
+            b = a + b
+            a = next_fib(i)
+        end do
+ 
+    end function next_fib
+
+end program fibonacci
---
+++ Programacao_Estruturada/slides/estruturas_e_funcoes/codes/selection.f90
@@ -0,0 +1,53 @@
+! Implementa o selection sort
+program selection
+
+    implicit none
+
+    integer, allocatable :: xs(:)
+    integer :: n
+
+    write(*,*) 'Insira o número de elementos: '
+    read(*,*) n
+
+    allocate(xs(n))
+
+    write(*,*) 'Insira os elementos do vetor: '
+    read(*,*) xs
+
+    call sort(n, xs)
+
+    write(*,*) xs
+
+    deallocate(xs)
+
+contains
+
+    subroutine sort(n, xs)
+
+        integer :: i, j, k, n, xs(:)
+
+        do i = 1, n - 1
+            j = i
+
+            do k = i + 1, n
+                if (xs(k) < xs(j)) then
+                    j = k
+                end if
+            end do
+
+            call swap(xs(i), xs(j))
+        end do
+
+    end subroutine sort
+
+    subroutine swap(x, y)
+
+        integer :: x, y, z
+
+        z = x
+        x = y
+        y = z
+
+    end subroutine swap
+
+end program selection
