9805044a973be3efef952463395ea0ab7538510f
Edson Alves <edsomjr@gmail.com>
2020-11-16 09:52:26 -0300

    Ajustes no comentário do código.

--- Programacao_Funcional/slides/funcoes/codes/frommaybe.hs
+++ Programacao_Funcional/slides/funcoes/codes/frommaybe.hs
@@ -1,4 +1,10 @@
--- Esta função está disponível na biblioteca Prelude
+-- Esta função está disponível na biblioteca Maybe.
+-- Para utilizá-la sem precisar da implementação abaixo,
+-- importe esta biblioteca com o comando
+--
+-- import Maybe
+--
+--
 -- Ela extrai o valor contido no tipo Maybe (wrapped),
 -- ou retorna defval, em caso Nothing
 fromMaybe defval wrapped =
