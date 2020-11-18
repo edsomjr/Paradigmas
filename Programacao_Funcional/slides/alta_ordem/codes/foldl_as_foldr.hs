3cab7cacd8e3787463e9582f4a36cfeeb5c5412d
Edson Alves <edsomjr@gmail.com>
2020-11-18 11:00:58 -0300

    Ajustes no texto.

--- Programacao_Funcional/slides/alta_ordem/codes/foldl_as_foldr.hs
+++ Programacao_Funcional/slides/alta_ordem/codes/foldl_as_foldr.hs
@@ -1,6 +1,5 @@
     accumulate [1..3] = foldl (+) 0 [1..3]
                       == foldr step id [1..3] 0
-                      == foldr step id [1..3] 0
                       == step 1 (foldr step id [2..3]) 0
                       == step 1 (step 2 (foldr step id [3..3])) 0
                       == step 1 (step 2 (step 3 (foldr step id []))) 0
