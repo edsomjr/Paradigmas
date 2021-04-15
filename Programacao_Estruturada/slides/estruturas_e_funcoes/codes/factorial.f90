f594a3f7c5a00000a4a488b2eb162a6940a98a6d
Edson Alves <edsomjr@gmail.com>
2021-04-15 19:25:12 -0300

    Correções de erro de digitação.

--- Programacao_Estruturada/slides/estruturas_e_funcoes/codes/factorial.f90
+++ Programacao_Estruturada/slides/estruturas_e_funcoes/codes/factorial.f90
@@ -3,15 +3,15 @@ program factorial
 
     implicit none
 
-    integer :: n, i, factorial = 1
+    integer :: n, i, Fn = 1
 
     read(*,*) n
 
     do i = 1, n
-        factorial = factorial * i
+        Fn = Fn * i
    end do
 
-    write(*,1) n, factorial
+    write(*,1) n, Fn
 
 1   format ('Fatorial de ', I2, I10)
 
