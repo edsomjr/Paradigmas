741da6808864ea5e90bf5a9488c9b913d1073565
Edson Alves <edsomjr@gmail.com>
2021-08-19 11:01:06 -0300

    Atualização do PDF.

--- Paradigma_Logico/slides/unificacao/codes/equals.pl
+++ Paradigma_Logico/slides/unificacao/codes/equals.pl
@@ -1,17 +1,12 @@
     ?- a = a.
     true.
-
     ?- a = b.
     false.
-
     ?- p(a, b) = p(a, b).
     true
-
     ?- p(a, b) = p(a, c).
     false.
-
     ?- x(y, z(i, j)) = x(y, z(i, j)).
     true.
-
     ?- x(y, z(i, j)) = x(y, z(j, i)).
     false.
