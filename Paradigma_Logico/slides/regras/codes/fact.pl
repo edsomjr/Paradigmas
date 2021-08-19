b062e57acee31fdebb514a92872009010337b9b6
Edson Alves <edsomjr@gmail.com>
2021-08-19 10:57:19 -0300

    Atualização do PDF.

--- Paradigma_Logico/slides/regras/codes/fact.pl
+++ Paradigma_Logico/slides/regras/codes/fact.pl
@@ -1,16 +1,13 @@
 % Implementação recursiva da função fatorial
 fact(F, 0) :- F is 1.
-
 fact(F, N) :-
     N > 0,
     NewN is N - 1,
     fact(X, NewN),
     F is X*N.
 
-
 % Implementação recursiva de cauda
 factTR(F, 0, Acc) :- F is Acc.
-
 factTR(F, N, Acc) :- 
     N > 0,
     NewN is N - 1,
