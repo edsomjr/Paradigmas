6df755a464b152ee69402e7fd87972805e410515
Edson Alves <edsomjr@gmail.com>
2025-05-16 20:58:11 -0300

    Ajustes no texto.

--- Programacao_Funcional/funcoes/codes/list_example.hs
+++ Programacao_Funcional/funcoes/codes/list_example.hs
@@ -1,13 +1,13 @@
-    -- Definição alternativa de lista
-    data List a = Cons a (List a)
-                | Null
-                deriving (Show)
+-- Definição alternativa de lista
+data List a = Cons a (List a)
+            | Null
+            deriving (Show)
 
-    -- Define uma lista do novo tipo
-    xs = Cons 1 (Cons 2 (Cons 3 Null))
+-- Define uma lista do novo tipo
+xs = Cons 1 (Cons 2 (Cons 3 Null))
 
-    -- Converte da lista definida para a lista padrão do Haskell
-    toList Null = []
-    toList (Cons x xs) = x : toList xs
+-- Converte da lista definida para a lista padrão do Haskell
+toList Null = []
+toList (Cons x xs) = x : toList xs
 
-    main = print (xs, toList xs)
+main = print (xs, toList xs)
